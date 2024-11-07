284c0d7a91c1d365b969fc7aa1993b54bffb28c0
Tatianadellareti <tatianadellareti02@gmail.com>
2024-11-07 18:53:50 -0300

    Add files via upload

---
+++ Atividade Pratica_3/script.js
@@ -0,0 +1,9 @@
+function colorirVermelho() {
+    const body = document.querySelector('body')
+    body.style.background = 'red'
+}
+
+function colorirAmarelo() {
+    const body = document.querySelector('body')
+    body.style.background = 'yellow'
+}
